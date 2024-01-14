cf0ce0fd6d0d85abf52d0079262715c39b4e0171
Ben Bld <bld.prom@gmail.com>
2024-01-15 00:26:38 +0100

    chore: initialisation projet

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./sass/main.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import * as logements from "./assets/logements.json";
+
+import { Header } from "./layout/Header";
+import { Footer } from "./layout/Footer";
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
+import { Asset } from "./pages/asset";
+import { Error404 } from "./pages/error404";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home assets={logements} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/asset/:id" element={<Asset />} />
+        <Route path="*" element={<Error404 />} /> {/* Route wildcard pour les URL non définies */}
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
