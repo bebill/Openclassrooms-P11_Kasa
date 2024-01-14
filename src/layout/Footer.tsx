d51a02c59b8d844be534d47b29dcb884a2f41f9c
Ben Bld <bld.prom@gmail.com>
2024-01-15 00:51:05 +0100

    chore: footer + scss

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <img src="/logo_footer.svg" alt="Logo Kasa Footer" />
+      <p>© {new Date().getFullYear()} Kasa. All rights reserved</p>
+    </footer>
+  );
+};
