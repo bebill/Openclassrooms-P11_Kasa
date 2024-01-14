e91dd475499babdbd2f38dc66660c5150be59b63
Ben Bld <bld.prom@gmail.com>
2024-01-15 00:50:06 +0100

    chore: homepage + scss

---
+++ src/pages/home.tsx
@@ -0,0 +1,25 @@
+import { AssetCardList } from "../components/AssetCardList";
+import { iAssetCard } from "../interfaces/assets";
+
+export const Home = ({ assets }: { assets: iAssetCard[] }) => {
+  const assetList = assets;
+  return (
+    <main>
+      <section className="hero">
+        <img
+          className="hero_img"
+          src="/hero_home.png"
+          alt="Entête page d'accueil"
+        />
+        <div className="hero_title">
+        <h1 className="title">Chez vous,&nbsp;</h1>
+        <h1 className="title">partout et ailleurs</h1>
+        </div>
+      </section>
+
+      <section className="gallery">
+        <AssetCardList assets={assetList} />
+      </section>
+    </main>
+  );
+};
